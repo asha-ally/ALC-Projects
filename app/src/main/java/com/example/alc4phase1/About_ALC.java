14fb6bb0310ea6184f51622d2b971bff48dd662a
Student <asherally079@gmail.com>
2019-07-15 10:07:36 +0300

    ALC Project

--- app/src/main/java/com/example/alc4phase1/About_ALC.java
+++ app/src/main/java/com/example/alc4phase1/About_ALC.java
@@ -4,9 +4,12 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.http.SslError;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
@@ -21,13 +24,27 @@ public class About_ALC extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about__alc);
         wView=findViewById(R.id.wView);
-        wView.setWebViewClient(new WebViewClient());
-        wView.getSettings().setJavaScriptEnabled(true);
         wView.loadUrl("https://andela.com/alc/");
+        wView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
 
-
+        wView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed();
             }
+        });
 
+      wView.getSettings().setJavaScriptEnabled(true);
+        wView.getSettings().setDomStorageEnabled(true);
+       wView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+       wView.getSettings().setLoadWithOverviewMode(true);
+        wView.getSettings().setUseWideViewPort(true);
+       wView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NORMAL);
+    }
 
 
 }
+
+
+
+
